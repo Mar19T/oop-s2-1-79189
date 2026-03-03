8337132ec0faffc7065038812d68624d89eec68e
Mario Tejeda <marioandredub@gmail.com>
2026-03-03 15:05:09 +0000

    Add all the classes to the Domain

--- Library.Domain/Books.cs
+++ Library.Domain/Books.cs
@@ -1,12 +1,17 @@
-﻿namespace Library.Domain
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
 {
-    public class Product
+    internal class Books
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string ISBN { get; set; }
         public string Category { get; set; }
         public bool IsAvailable { get; set; }
-
     }
 }
---
+++ Library.Domain/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    internal class Loan
+    {
+        public string id { get; set; }
+        public string BookID { get; set; }
+        public string MemberID{ get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
